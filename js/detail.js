bdf2dd82e3b4a081686a8ee580bbe86208f2c9b2
NODUF <nitikvideos@gmail.com>
2021-11-04 22:23:58 +0100

    0.0.7

--- js/detail.js
+++ js/detail.js
@@ -3,15 +3,15 @@
 //DATA
 var post=[
   {
-      title: "WANT SOME HERS COFFEE?",
-      datecreated:"10/27/21",
-      toolsused:"Blender, Photoshop",
-      originalresolution:"2000 x 2500",
-      filesize: "4,460,355 bytes",
-      nftart: "nfts/want-some-hers-coffee.png",
-      href: "want-some-hers-coffee",
-      link: "https://rarible.com/token/0xf6793da657495ffeff9ee6350824910abc21356c:104238516375740187513133738248866004109794379427696023384912212459279381168141?tab=details"
-  },
+    title: "SUNNY DAY WITH MASTER CHIEF",
+    datecreated:"10/17/21",
+    toolsused:"Blender",
+    originalresolution:"2000 x 2500",
+    filesize: "1,882,181 bytes",
+    nftart: "nfts/sunny-day-with-master-chief.png",
+    href: "sunny-day-with-master-chief",
+    link: "https://rarible.com/token/0xf6793da657495ffeff9ee6350824910abc21356c:104238516375740187513133738248866004109794379427696023384912212459279381168133?tab=details"
+},
   {
       title: "BANANA ISLAND?!",
       datecreated:"10/26/21",
@@ -23,15 +23,15 @@ var post=[
       link: "https://rarible.com/token/0xf6793da657495ffeff9ee6350824910abc21356c:104238516375740187513133738248866004109794379427696023384912212459279381168140?tab=details"
   },
   {
-      title: "SUNNY DAY WITH MASTER CHIEF",
-      datecreated:"10/17/21",
-      toolsused:"Blender",
-      originalresolution:"2000 x 2500",
-      filesize: "1,882,181 bytes",
-      nftart: "nfts/sunny-day-with-master-chief.png",
-      href: "sunny-day-with-master-chief",
-      link: "https://rarible.com/token/0xf6793da657495ffeff9ee6350824910abc21356c:104238516375740187513133738248866004109794379427696023384912212459279381168133?tab=details"
-  },
+    title: "WANT SOME HERS COFFEE?",
+    datecreated:"10/27/21",
+    toolsused:"Blender, Photoshop",
+    originalresolution:"2000 x 2500",
+    filesize: "4,460,355 bytes",
+    nftart: "nfts/want-some-hers-coffee.png",
+    href: "want-some-hers-coffee",
+    link: "https://rarible.com/token/0xf6793da657495ffeff9ee6350824910abc21356c:104238516375740187513133738248866004109794379427696023384912212459279381168141?tab=details"
+},
 ];
   
 //Loop length:
